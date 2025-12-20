69c6835f4d71a3f084a54b8e4e360267cb4deecd
km-saifullah <kmsaifullah24@gmail.com>
2025-12-20 10:53:40 +0600

    get single user feature added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import { AppError } from "../utils/AppError";
 import cloudinaryUpload from "../utils/cloudinaryUpload";
 import cloudinaryDelete from "../utils/cloudinaryDelete";
 import { AuthenticatedRequest } from "../middlewares/isLoggedIn";
+import { User } from "../models/user.model";
 
 // update user profile
 export const updateUserProfile = async (
@@ -40,6 +41,71 @@ export const updateUserProfile = async (
       status: true,
       statusCode: 200,
       message: "Profile updated successfully",
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//  get all users
+export const getAllUsers = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const loggedInUser = req.user;
+
+    if (!loggedInUser) {
+      throw new AppError("Unauthorized access", 401);
+    }
+
+    if (loggedInUser.role !== "admin") {
+      throw new AppError("Access denied", 403);
+    }
+
+    const users = await User.find()
+      .select("-password -refreshToken -otp -otpExpire")
+      .sort({ createdAt: -1 });
+
+    return res.status(200).json({
+      status: true,
+      statusCode: 200,
+      message: "Users fetched successfully",
+      data: users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// get specific user 
+export const getSingleUser = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const loggedInUser = req.user;
+    const { id } = req.params;
+
+    if (!loggedInUser) {
+      throw new AppError("Unauthorized access", 401);
+    }
+
+    const user = await User.findById(id).select(
+      "-password -refreshToken -otp -otpExpire"
+    );
+
+    if (!user) {
+      throw new AppError("User not found", 404);
+    }
+
+    return res.status(200).json({
+      status: true,
+      statusCode: 200,
+      message: "User fetched successfully",
       data: user,
     });
   } catch (error) {
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { updateUserProfile } from "../controllers/user.controller";
+import { getSingleUser, updateUserProfile } from "../controllers/user.controller";
 import { isLoggedIn } from "../middlewares/isLoggedIn";
 import { uploadSingle } from "../utils/upload";
 
@@ -8,5 +8,7 @@ const router = Router();
 router
   .route("/profile")
   .put(isLoggedIn, uploadSingle("avatar"), updateUserProfile);
+
+router.route("/:id").get(isLoggedIn, getSingleUser)
 
 export default router;
